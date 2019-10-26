0290046478ef6aadfb0fc73af474814a76829451
huangshuai <huangshuai@qutoutiao.net>
2019-10-26 19:20:11 +0800

    a

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bufio"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
 )
 
-var clients = make(map[int]net.Conn)
+var clients = make(map[int]*net.TCPConn)
 
 func main() {
 	listener, err := net.ListenTCP("tcp", &net.TCPAddr{Port: 8080})
@@ -20,28 +20,59 @@ func main() {
 		if err != nil {
 			log.Fatal("tcp server accept error", err)
 		}
-		conn.SetKeepAlive(true)
-		conn.SetKeepAlivePeriod(60 * 1000000000)
 		go handleConnection(conn)
 	}
 }
 
 func handleConnection(conn *net.TCPConn) {
-	bufferBytes, err := bufio.NewReader(conn).ReadBytes('\n')
-
-	if err != nil {
-		log.Println("client left..")
-		conn.Close()
-		return
+	defer conn.Close()
+	conn.SetKeepAlive(true)
+	conn.SetKeepAlivePeriod(60 * 1000000000)
+	buffer := make([]byte, 1024)
+	channel := make(chan byte, 128)
+	for {
+		pos := 0
+		for pos < 4 {
+			n, err := conn.Read(buffer[pos:4])
+			if err != nil {
+				log.Println("broken link")
+				return
+			}
+			pos += n
+		}
+		len := int(binary.BigEndian.Uint32(buffer[0:4]))
+		if len < 5 || len > 1024 {
+			conn.Write([]byte(fmt.Sprintf("invalid len %v", len)))
+			log.Println("invalid len:", len)
+			return
+		}
+		log.Println("len:", len, buffer)
+		for pos < len {
+			n, err := conn.Read(buffer[pos:len])
+			if err != nil {
+				log.Println("broken link")
+				return
+			}
+			pos += n
+		}
+		log.Println("len:", len, buffer)
 	}
+	// bufferBytes, err := bufio.NewReader(conn).ReadBytes('\n')
 
-	message := string(bufferBytes)
-	clientAddr := conn.RemoteAddr().String()
-	response := fmt.Sprintf(message + " from " + clientAddr + "\n")
+	// if err != nil {
+	// 	log.Println("client left..")
 
-	log.Println(response)
+	// 	conn.Close()
+	// 	return
+	// }
 
-	conn.Write([]byte("you sent: " + response))
+	// message := string(bufferBytes)
+	// clientAddr := conn.RemoteAddr().String()
+	// response := fmt.Sprintf(message + " from " + clientAddr + "\n")
 
-	handleConnection(conn)
+	// log.Println(response)
+
+	// conn.Write([]byte("you sent: " + response))
+
+	// handleConnection(conn)
 }
